da1610183439fb4d3ba235a70569f00c08fe4fb4
Sandra Heinzová <heinzova.sandra@gmail.com>
2024-07-24 23:09:32 +0200

    fixed filtering query for menu for new registered users

--- main.py
+++ main.py
@@ -101,7 +101,7 @@ def get_filtered_query(user_preferences, value):
         if user_preferences.vegetarian:
             query = query.where(Meal.vegetarian)
 
-        return query
+    return query
 
 
 @app.route("/menu")
